35f519f164ccf0380984579342de7a1aa7be1e17
sunandanthakur <sunandanthakur@Sunandans-MacBook-Air.local>
2026-08-11 17:35:24 +0530

    Remove old Conor, Arman, and Jon Jones Fan Zone entries and images

--- src/app.supabase.js
+++ src/app.supabase.js
@@ -146,9 +146,7 @@ const fanWallpapers = [
   { id: "conor-mcgregor-stance", name: "Conor McGregor", nickname: "The Notorious", title: "Champ Champ Stance", style: "Boxing, Taekwondo", record: "22-6-0", src: "assets/fan-conor-mcgregor-stance.jpg", position: "center 20%" },
   { id: "lawler-macdonald", name: "UFC 189", nickname: "The Blood Bath", title: "Lawler vs MacDonald II", style: "Iconic Striking War", record: "Lawler 30-16 · Rory 23-10-1", src: "assets/fan-lawler-macdonald.jpg", position: "center 20%" },
   { id: "tony-ferguson", name: "Tony Ferguson", nickname: "El Cucuy", title: "Boogeyman of 155", style: "10th Planet BJJ, Chaos", record: "25-11-0", src: "assets/fan-tony-ferguson.jpg", position: "center 20%" },
-  { id: "conor-mcgregor-stool", name: "Conor McGregor", nickname: "The Notorious", title: "Focused on the Stool", style: "Boxing, Taekwondo", record: "22-6-0", src: "assets/fan-conor-mcgregor-stool.jpg", position: "center 20%" },
-  { id: "arman-tsarukyan", name: "Arman Tsarukyan", nickname: "Ahalkalakets", title: "Ready for War", style: "Freestyle Wrestling", record: "22-3-0", src: "assets/fan-arman-tsarukyan.jpg", position: "center 20%" },
-  { id: "jon-jones-crouch", name: "Jon Jones", nickname: "Bones", title: "Predator Stance", style: "Wrestling, Muay Thai, BJJ", record: "28-1-0 (1 NC)", src: "assets/fan-jon-jones-crouch.jpg", position: "center 20%" },
+
   { id: "yoel-romero-smile", name: "Yoel Romero", nickname: "Soldier of God", title: "See You Soon Boi", style: "Freestyle Wrestling", record: "16-7-0", src: "assets/fan-yoel-romero.jpg", position: "center 20%" },
   { id: "demetrious-johnson", name: "Demetrious Johnson", nickname: "Mighty Mouse", title: "Championship Glory", style: "Freestyle Wrestling, Muay Thai, BJJ", record: "34-4-1", src: "assets/fan-demetrious-johnson.png", position: "center 30%" },
   { id: "daniel-cormier", name: "Daniel Cormier", nickname: "DC", title: "Double Champion", style: "Freestyle Wrestling, Kickboxing", record: "22-3-0 (1 NC)", src: "assets/fan-daniel-cormier.png", position: "center 25%" },
